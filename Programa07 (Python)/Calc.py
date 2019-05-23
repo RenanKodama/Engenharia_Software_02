6d3ed5b3bbbcef20ef77b46563a4d40714d1207c
Renan <renan_kdm_rdg@hotmail.com>
2019-05-23 04:16:36 -0300

    Intervalo de Previsão
    Falta: 
    1. Calcule Range para o intervalo de 70% das previsões..
    2. Calcule o limite superior do intervalo (UPI) como y Range(70%) k  .
    3. Calcule o limite inferior do intervalo (LPI) como y Range(70%) k  .

--- Programa07 (Python)/Calc.py
+++ Programa07 (Python)/Calc.py
@@ -5,11 +5,15 @@
     Renan Kodama Rodrigues 1602098
 '''
 
+from Simspon import Simspon
+
+
 class Caculos:
-    def __init__(self, data_Hash, coluns):
+    def __init__(self, data_Hash, coluns, xk):
         self.data_Hash = data_Hash
         self.coluns = coluns
         self.size = len(self.data_Hash[self.coluns[0]])
+        self.xk = xk
 
     # recycled and changed
     def somatorio_XY(self):
@@ -55,11 +59,14 @@ class Caculos:
         return [x_quadrado, y_quadrado]
 
     # recycled and changed
-    def coeficiente_Correlacao(self):
-        x_y = multiplica_XY()
-        [sum_x, sum_y] = somatorio_XY()
-        [x_quad, y_quad] = quadrado_XY()
+    def media_XY(self, sum_x, sum_y):
+        avg_x = (sum_x) / len(self.data_Hash[self.coluns[0]])
+        avg_y = (sum_y) / len(self.data_Hash[self.coluns[1]])
 
+        return [avg_x, avg_y]
+
+    # recycled and changed
+    def coeficiente_Correlacao(self, x_y, sum_x, sum_y, x_quad, y_quad):
         part1 = (self.size * x_y) - (sum_x * sum_y)
         part2 = (
             ((self.size * x_quad) - (sum_x ** 2)) *
@@ -68,8 +75,36 @@ class Caculos:
 
         return (part1 / (part2 ** 0.5))
 
+    # recycled and changed
+    def regressao_Linear(self, x_y, avg_x, avg_y, x_quad):
+        beta_1 = (
+            (x_y - (self.size * avg_x * avg_y)) /
+            (x_quad - (self.size * (avg_x * avg_x)))
+            )
+        beta_0 = avg_y - (beta_1 * avg_x)
+
+        return [beta_0, beta_1]
+    
+    def encontrar_YK(self, beta_0, beta_1):
+        return (beta_0 + (beta_1 * self.xk))
+
     def significancia_Correlacao(self, val_corr):
         part1 = abs(val_corr) * ((self.size - 2) ** (0.5))
         part2 = (1 - (val_corr ** 2)) ** 0.5
+        x = part1 / part2
+        p = Simspon(0, x, (self.size - 2))
+        
+        # return (part1 / part2)
 
-        return (part1 / part2)
+
+    def calcular(self):
+        x_y = multiplica_XY()
+        [sum_x, sum_y] = somatorio_XY() 
+        [x_quad, y_quad] = quadrado_XY()
+        coef_corr = coeficiente_Correlacao(x_y, sum_x, sum_y, x_quad, y_quad)
+
+        # sign_Corr = significancia_Correlacao(coef_corr)
+
+        [avg_x, avg_y] = media_XY(sum_x, sum_y)
+        [beta_0, beta_1] = regressao_Linear(x_y, avg_x, avg_y, x_quad)
+        yk = encontrar_YK(beta_0, beta_1)
